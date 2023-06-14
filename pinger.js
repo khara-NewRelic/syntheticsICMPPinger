b64c998ebc7b2c855e85c3ba289cf99eeaf0a771
Kenichiro Hara <khara@newrelic.com>
2023-06-14 17:02:00 +0900

    ファーストコミット

---
+++ pinger.js
@@ -0,0 +1,92 @@
+// https://github.com/newrelic/quickstarts-synthetics-library/blob/main/library/ICMP/script.js
+
+var assert = require('assert');
+var ping = require('net-ping');
+
+var session = ping.createSession({
+  retries: 2,
+  timeout: 1000
+});
+
+var checksCompleted = 0;
+
+// Hosts are numbers in this example, but this could be replaced with a meaningful label for each host instead.
+var checks = [
+  { 'host': 'Google DNS', 'ip': '8.8.8.8' },
+  { 'host': '2', 'ip': '255.255.255.254' },
+  { 'host': '3', 'ip': '255.255.255.255' },
+  { 'host': 'Google', 'ip': '172.217.175.110' },
+  { 'host': 'Yahoo Japan', 'ip': '183.79.135.206' },
+  { 'host': 'Facebook', 'ip': '31.13.82.36' },
+  { 'host': 'twitter.com', 'ip': '104.244.42.193' },
+  { 'host': 'amazon.co.jp', 'ip': '52.119.161.5' },
+  { 'host': 'rakuten.co.jp', 'ip': '133.237.16.234' },
+  { 'host': 'apple.com', 'ip': '17.253.144.10' },
+ ]
+
+// Event API Initialization section
+//Define your authentication credentials.
+var myAccountID = $secure.MYACCOUNTID;
+var myAPIKey = $secure.MYUSERAPIKEY;
+
+var options = {
+  //Define endpoint URL.
+  url: "https://insights-collector.newrelic.com/v1/accounts/"+myAccountID+"/events",
+  //Define body of POST request.
+  body: '',
+  //Define New Relic API key and expected data type.
+  headers: {
+      'Api-Key': myAPIKey,
+      'Content-Type': 'application/json'
+      }
+};
+
+function checkEndpoint(item) {
+  session.pingHost(item.ip, function (error, target, sent, rcvd) {
+    var timeRequired = rcvd - sent;
+    var result = '';
+    if (error) {
+      //var timeRequired = rcvd - sent
+      console.log('ERROR - Ping failed for host ' + item.host + ' (IP ' + item.ip + ') with error: ' + error);
+      result = 'failed';
+
+      //Event APIを投げるためのBodyを生成する
+      //options.body = '[{"eventType":"CustomSyntheticsICMPPolling", "Host Name":\"'+ item.host +'\", "Host IP":\"' + item.ip + '\", "Result":"failed","Reason":\"' + error + '\","ResponseTime":'+timeRequired+'}]';
+    }
+    else {
+      //var timeRequired = rcvd - sent
+      console.log('Ping successful for host ' + item.host + ' (IP ' + item.ip + '). Response (ms): ' + timeRequired)
+      result = 'success';
+
+      //Event APIを投げるためのBodyを生成する
+      //options.body = '[{"eventType":"CustomSyntheticsICMPPolling", "Host Name":\"'+ item.host +'\", "Host IP":\"' + item.ip + '\", "Result":"success","Reason":\"' + error + '\","ResponseTime":'+timeRequired+'}]';
+    }
+    //Event APIを投げるためのBodyを生成する
+    options.body = '[{"eventType":"CustomSyntheticsICMPPolling", "Host Name":\"'+ item.host +'\", "Host IP":\"' + item.ip + '\", "Result":\"'+ result +'\","Reason":\"' + error + '\","ResponseTime":'+timeRequired+'}]';
+    //ここでEvent APIを投げる
+    console.log(options.body);
+    $http.post(options, callback);
+
+    checksCompleted++
+    if (checksCompleted == checks.length) {
+      session.close();
+      console.log("All checks were successful.");
+    }
+    else {
+      console.log("More checks to complete. Continuing...");
+    }
+  })
+}
+
+//Define expected results using callback function.
+function callback(error, response, body) {
+  //Log status code to Synthetics console.
+  console.log(response.statusCode + " status code of Event API Response");
+  //Verify that `body` contains element named `success` with a value of `true`.
+  //console.log("BODY: " + body);
+  //Log end of script.
+  console.log("End reached");
+}
+
+// Check each endpoint
+checks.forEach(checkEndpoint);
